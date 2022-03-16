f19fd54a2e298cee865035deff0b89038f3cb005
Yuriy Kholoimov <yurakholoimov@ya.ru>
2022-03-16 17:15:37 +0300

    Updates

---
+++ precache-manifest.51c88cfb419f62fe9b976303cff27b09.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f120914369ad5c82aa385906882db4ff",
+    "url": "/task-board/index.html"
+  },
+  {
+    "revision": "b2809c060bee404069be",
+    "url": "/task-board/static/css/main.adab7465.chunk.css"
+  },
+  {
+    "revision": "3e65d98137ca949be3ac",
+    "url": "/task-board/static/js/2.9b8968ac.chunk.js"
+  },
+  {
+    "revision": "ea2c7d1ff4b476b07b3445b73be3d513",
+    "url": "/task-board/static/js/2.9b8968ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2809c060bee404069be",
+    "url": "/task-board/static/js/main.5cccba54.chunk.js"
+  },
+  {
+    "revision": "4cf3ab76eeca03556bb3",
+    "url": "/task-board/static/js/runtime-main.085bb110.js"
+  },
+  {
+    "revision": "adc91222736438e4db1fd9d3c3090475",
+    "url": "/task-board/static/media/pexels-grey.adc91222.jpg"
+  }
+]);
